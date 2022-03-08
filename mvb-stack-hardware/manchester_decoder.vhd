658e380ea2c49fc43402619f468f837637b81d66
mozerj2001 <mozerj200@gmail.com>
2022-03-08 20:35:45 +0100

    decodes continued asynchronous manchester stream

--- mvb-stack-hardware/manchester_decoder.vhd
+++ mvb-stack-hardware/manchester_decoder.vhd
@@ -6,12 +6,13 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity manchester_decoder is
-    Port ( clk_xx : in  std_logic;										-- 16x clock input for clock recovery and oversampling
-			  rst : in std_logic;
-			  rdn : in std_logic;											-- control signal initiates read operation
-           manchester_in : in  std_logic;								-- incoming serial manchester-coded data
-           decoded_out : out  unsigned(7 downto 0);				-- outgoing data word
-			  data_ready : out std_logic									-- indicates that the decoded_out data is ready
+    Port ( clk_xx : in  				std_logic;								-- 16x clock input for clock recovery and oversampling
+			  rst : in 						std_logic;
+			  rdn : in 						std_logic;								-- control signal initiates read operation
+           manchester_in : in  		std_logic;								-- incoming serial manchester-coded data
+           decoded_out : out  		unsigned(7 downto 0);				-- outgoing data word
+			  data_ready : out 			std_logic;								-- indicates that the decoded_out data is ready
+			  decode_error : out			std_logic								-- an error has occured in the decode process (e. g. there was no edge mid-bit)
 			  );								
 end manchester_decoder;
 
@@ -26,38 +27,77 @@ constant SAMPLING_COUNTER_WIDTH : integer := 4;						-- width of the counter, ba
 ---------------------- INTERNAL SIGNALS -----------------------
 ---------------------------------------------------------------
 
--- internal register for edge detection (cells will be XOR-ed)
-signal man_data_in1 : std_logic_vector(1 downto 0);
+-- internal shift register for decoded input, value of current decoded bit
+signal man_data_in_shr : std_logic_vector(7 downto 0);
+signal current_bit_decoded : std_logic;
 
--- internal 1x clock signal and clock enable
-signal clk1x : std_logic;
-signal clk1x_en : std_logic;
+-- register for edge direction detection
+signal input_edge_register : std_logic_vector(1 downto 0);
 
 -- counter for sampling at 25% clk and 75% clk and sample enable signal
-signal fourth_counter : unsigned(SAMPLING_COUNTER_WIDTH downto 0);
+signal fourth_counter : unsigned(SAMPLING_COUNTER_WIDTH-1 downto 0);
+signal fourth_counter_at_edge : unsigned(SAMPLING_COUNTER_WIDTH-1 downto 0) := to_unsigned(2**SAMPLING_COUNTER_WIDTH - 1, SAMPLING_COUNTER_WIDTH);		-- register to save counter value at edge for sync
 signal sample_manchester_input : std_logic;
+		--variable sample_at_25 : boolean;
+		--variable sample_at_75 : boolean;
+signal sample_at_25 : std_logic := '0';
+signal sample_at_75 : std_logic := '1';
 
 ---------------------------------------------------------------
 ------------------- BEHAVIORAL DESCRIPTION --------------------
 ---------------------------------------------------------------
 begin
-
--- create counter based on which sampling times can be determined
-process(rst,  clk_xx)
+-- get input bit into shift register
+process(rst, sample_manchester_input)
 begin
 	if(rst = '1') then
-		fourth_counter <= to_unsigned(0, SAMPLING_COUNTER_WIDTH);
+		input_edge_register <= "00";
+	elsif(sample_manchester_input = '1') then
+		input_edge_register <= (input_edge_register(0) & manchester_in);
 	else
-		fourth_counter <= fourth_counter + 1;
 	end if;
-		
 end process;
 
+-- create counter, based on which sampling times can be determined,
+-- 	save currently decoded bit value when the clock cycle comes to an end (LSB FIRST)
+process(clk_xx)
+begin
+	if(rising_edge(clk_xx)) then
+		if(rst = '1') then
+			fourth_counter <= to_unsigned(0, SAMPLING_COUNTER_WIDTH);
+			man_data_in_shr(7 downto 0) <= "00000000";
+		elsif(fourth_counter = fourth_counter_at_edge) then
+			fourth_counter <= to_unsigned(0, SAMPLING_COUNTER_WIDTH);
+			man_data_in_shr(7 downto 0) <= (current_bit_decoded & man_data_in_shr(7 downto 1));
+		else
+			fourth_counter <= fourth_counter + 1;
+		end if;
+	end if;	
+end process;
+
+-- get edge direction of current bit, save value according to the manchester coding standard
+-- save the value of the sampling counter for synchronization purposes
+p_DETECT_EDGE : process (input_edge_register)
+begin
+	case input_edge_register is
+		when "10" =>
+			current_bit_decoded <= '1';
+		when "01" =>
+			current_bit_decoded <= '0';
+		when others =>
+	end case;
+	if((fourth_counter > to_unsigned(OVERSAMPLING_FACTOR/4, SAMPLING_COUNTER_WIDTH)) and (fourth_counter < to_unsigned(OVERSAMPLING_FACTOR*3/4, SAMPLING_COUNTER_WIDTH))) then
+		fourth_counter_at_edge <= fourth_counter;
+	end if;
+end process p_DETECT_EDGE;
 
 
--- sample value at clk3 and clk11
-sample_manchester_input <= '1' when (fourth_counter = to_unsigned(OVERSAMPLING_FACTOR*3/4, SAMPLING_COUNTER_WIDTH))
-	or (fourth_counter = to_unsigned(OVERSAMPLING_FACTOR*3/4, SAMPLING_COUNTER_WIDTH)) else '0';
+-- sample value at clk3 and clk11 (at 25% and 75%)
+		--sample_at_25 := (fourth_counter = to_unsigned(OVERSAMPLING_FACTOR/4, SAMPLING_COUNTER_WIDTH));
+		--sample_at_75 := (fourth_counter = to_unsigned(OVERSAMPLING_FACTOR*3/4, SAMPLING_COUNTER_WIDTH));
+sample_at_25 <= '1' when (fourth_counter = to_unsigned(OVERSAMPLING_FACTOR/4, SAMPLING_COUNTER_WIDTH)) else '0';
+sample_at_75 <= '1' when (fourth_counter = to_unsigned(OVERSAMPLING_FACTOR*3/4, SAMPLING_COUNTER_WIDTH)) else '0';
+sample_manchester_input <= '1' when (sample_at_25 = '1') or (sample_at_75 = '1') else '0';
 
 end Behavioral;
 
--- mvb-stack-hardware/test_manchester_decoder.vhd
+++ mvb-stack-hardware/test_manchester_decoder.vhd
@@ -1,3 +1,4 @@
+
 -- Testbench for the iSim simualtion of the Manchester Decoder
 
 LIBRARY ieee;
@@ -10,21 +11,27 @@ END testbench;
 ARCHITECTURE behavior OF testbench IS
 
 	component manchester_decoder is
-		Port(clk_xx : in  std_logic;
-			  rst : in std_logic;
-			  rdn : in std_logic;
-			  manchester_in : in  std_logic;
+		Port(clk_xx : in  		std_logic;
+			  rst : in 				std_logic;
+			  rdn : in 				std_logic;
+			  manchester_in : in	std_logic;
 			  decoded_out : out  unsigned(7 downto 0);		
 			  data_ready : out	std_logic
 			  );
 	end component manchester_decoder;
 	
-	signal clk : std_logic;
-	signal rst : std_logic;
-	signal rdn : std_logic;
-	signal manchester_in : std_logic;
-	signal decoded_out : unsigned(7 downto 0);
-	signal data_ready : std_logic;
+	constant clk_period : time := 10 ns;
+	-- Random test constant from the internet 10100111001 (for now)
+	constant test_manchester_code : std_logic_vector(21 downto 0) := "1001100101101010010110";
+	signal i : integer := 0;
+	
+	signal clk_xx : std_logic := '0';
+	signal rst : std_logic := '0';
+	signal rdn : std_logic := '0';
+	signal manchester_in : std_logic := '0';
+	signal decoded_out : unsigned(7 downto 0) := "00000000";
+	signal data_ready : std_logic := '0';
+	signal input_sync_counter : unsigned(2 downto 0) := to_unsigned(0, 3);
 	
 	
 BEGIN
@@ -32,27 +39,55 @@ BEGIN
 
 -- Component Instantiation
 		 tested_decoder: manchester_decoder PORT MAP(
-					clk_xx => clk,
+					clk_xx => clk_xx,
 					rst => rst,
 					rdn => rdn,
 					manchester_in => manchester_in,
 					decoded_out => decoded_out,
 					data_ready => data_ready
 		 );
+		 
+	-- Generate clock signal
+	clk_gen : process
+	begin
+		clk_xx <= '0';
+		wait for clk_period/2;
+		clk_xx <= '1';
+		wait for clk_period/2;
+	end process clk_gen;
+	
+	-- count a sending period, because it didin't work with a wait statement
+	sending_sync : process (clk_xx)
+	begin
+		if(rising_edge(clk_xx)) then
+			if((input_sync_counter = to_unsigned(7, 3)) or (rst = '1')) then input_sync_counter <= to_unsigned(0, 3);
+			else input_sync_counter <= input_sync_counter + 1;
+			end if;
+		end if;
+	end process sending_sync;
+	
+	-- Generate manchester coded serial input
+	manchester_gen : process (clk_xx)
+	begin
+		if(rst = '0') then
+				if(rising_edge(clk_xx) and (input_sync_counter = to_unsigned(0, 3))) then
+					manchester_in <= test_manchester_code(i);
+					i <= i + 1;
+				end if;
+				if(i = 21) then i <= 0; end if;
+		end if;
+	end process manchester_gen;
 
 
 	--  Test Bench Statements
 	tb : PROCESS
 	BEGIN
-		-- Reset and Clock
+		-- Manual reset
 		rst <= '1', '0' after 100 ns;
-		clk <= not clk after 5 ns;
 
-		-- Test stimulus
-		
 
 		wait; -- will wait forever
 	END PROCESS tb;
 --  End Test Bench 
 
-END;
+END behavior;
